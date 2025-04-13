5363556fecd3071348ae92faa837e00d8b410b58
Croubs <diegoantoniolopezruelas@gmail.com>
2025-04-12 22:08:35 -0600

    Se separó la obtención de los índices del mensaje y el mensaje apartir de los índices en funciones separadas

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-
-
-
-def cifradoCesar(m:str, n:int):
+# Para obtener los indices del alfabeto de 26 letras y saber cuáles son mayúsculas
+def getIndexes(m:str):
   alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  print(n)
   msg = list(m)
 
-  isLower = list(map(lambda c: c.isupper(), msg))
-  cesar = list(map(lambda c: alphabet[(alphabet.index(c.lower()) + n) % 26], msg))
-  encrypted = list(map(lambda c, l: c.upper() if l else c, cesar, isLower))
+  isUpper = list(map(lambda c: c.isupper(), msg))
+  indexes = list(map(lambda c: alphabet.index(c.lower()), msg))
 
-  return ''.join(encrypted)
+  return (indexes, isUpper)
+
+# Para transformar los indices del alfabeto en letras y devolver el mensaje como un string
+def getMessage(indexes:list, isUpper:list):
+  alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  msg = list(map(lambda i, u: alphabet[i].upper() if u else alphabet[i], indexes, isUpper))
+
+  return ''.join(msg)
 
 
-msg = input('Mensaje: ')
-n = int(input('Desplazamiento: '))
+# Para desencriptar el mensaje hacer -n
+def cifradoCesar(m:str, n:int):
+  (msg, isUpper) = getIndexes(m)
+  cesar = list(map(lambda c: (c + n) % 26, msg))
 
-encrypted = cifradoCesar(msg, n)
-print(encrypted)
-print(cifradoCesar(encrypted, -n))
+  return getMessage(cesar, isUpper)
+
